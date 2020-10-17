09042ea9c99f9fe2cab7218a96ce264e06893bdb
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-10-17 19:42:56 -0300

    inicio
    inicio do projeto

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p class="rodape">©Todos os direitos reservados.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+@import "../assets/css/estilo.min.css";
+</style>
