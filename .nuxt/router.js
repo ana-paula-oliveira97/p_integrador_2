da1eac819915763461c6ab6f412dd66450a7c51b
Débora Santos <54008405+Deboradso@users.noreply.github.com>
2020-10-19 17:06:17 -0300

    estilo.scss

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,17 +3,17 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _17215da5 = () => interopDefault(import('..\\pages\\a_gangue.vue' /* webpackChunkName: "pages/a_gangue" */))
-const _70abef19 = () => interopDefault(import('..\\pages\\about.vue' /* webpackChunkName: "pages/about" */))
-const _08902392 = () => interopDefault(import('..\\pages\\debuts.vue' /* webpackChunkName: "pages/debuts" */))
-const _22212b48 = () => interopDefault(import('..\\pages\\escolhida.vue' /* webpackChunkName: "pages/escolhida" */))
-const _2f3c33ab = () => interopDefault(import('..\\pages\\morte_subita_2.vue' /* webpackChunkName: "pages/morte_subita_2" */))
-const _ea6ba0d4 = () => interopDefault(import('..\\pages\\morteS.vue' /* webpackChunkName: "pages/morteS" */))
-const _5d86ef1e = () => interopDefault(import('..\\pages\\poster.vue' /* webpackChunkName: "pages/poster" */))
-const _144b810a = () => interopDefault(import('..\\pages\\sangue.vue' /* webpackChunkName: "pages/sangue" */))
-const _1d9ff9ea = () => interopDefault(import('..\\pages\\sangue_de_pelicano.vue' /* webpackChunkName: "pages/sangue_de_pelicano" */))
-const _0c127873 = () => interopDefault(import('..\\pages\\tickets.vue' /* webpackChunkName: "pages/tickets" */))
-const _df386c44 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _2eb4ba67 = () => interopDefault(import('..\\pages\\a_gangue.vue' /* webpackChunkName: "pages/a_gangue" */))
+const _17799997 = () => interopDefault(import('..\\pages\\about.vue' /* webpackChunkName: "pages/about" */))
+const _2e9f9379 = () => interopDefault(import('..\\pages\\debuts.vue' /* webpackChunkName: "pages/debuts" */))
+const _6c70b44c = () => interopDefault(import('..\\pages\\escolhida.vue' /* webpackChunkName: "pages/escolhida" */))
+const _3f591226 = () => interopDefault(import('..\\pages\\morte_subita_2.vue' /* webpackChunkName: "pages/morte_subita_2" */))
+const _849c5650 = () => interopDefault(import('..\\pages\\morteS.vue' /* webpackChunkName: "pages/morteS" */))
+const _04242db3 = () => interopDefault(import('..\\pages\\poster.vue' /* webpackChunkName: "pages/poster" */))
+const _28c1e4bd = () => interopDefault(import('..\\pages\\sangue.vue' /* webpackChunkName: "pages/sangue" */))
+const _1ae05566 = () => interopDefault(import('..\\pages\\sangue_de_pelicano.vue' /* webpackChunkName: "pages/sangue_de_pelicano" */))
+const _361f7b71 = () => interopDefault(import('..\\pages\\tickets.vue' /* webpackChunkName: "pages/tickets" */))
+const _3731745c = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -33,47 +33,47 @@ export const routerOptions = {
 
   routes: [{
     path: "/a_gangue",
-    component: _17215da5,
+    component: _2eb4ba67,
     name: "a_gangue"
   }, {
     path: "/about",
-    component: _70abef19,
+    component: _17799997,
     name: "about"
   }, {
     path: "/debuts",
-    component: _08902392,
+    component: _2e9f9379,
     name: "debuts"
   }, {
     path: "/escolhida",
-    component: _22212b48,
+    component: _6c70b44c,
     name: "escolhida"
   }, {
     path: "/morte_subita_2",
-    component: _2f3c33ab,
+    component: _3f591226,
     name: "morte_subita_2"
   }, {
     path: "/morteS",
-    component: _ea6ba0d4,
+    component: _849c5650,
     name: "morteS"
   }, {
     path: "/poster",
-    component: _5d86ef1e,
+    component: _04242db3,
     name: "poster"
   }, {
     path: "/sangue",
-    component: _144b810a,
+    component: _28c1e4bd,
     name: "sangue"
   }, {
     path: "/sangue_de_pelicano",
-    component: _1d9ff9ea,
+    component: _1ae05566,
     name: "sangue_de_pelicano"
   }, {
     path: "/tickets",
-    component: _0c127873,
+    component: _361f7b71,
     name: "tickets"
   }, {
     path: "/",
-    component: _df386c44,
+    component: _3731745c,
     name: "index"
   }],
 
