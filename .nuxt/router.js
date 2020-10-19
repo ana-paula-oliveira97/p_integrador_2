ee8a7e3a0de7a567b2377bdc64948d29c56977f0
Débora Santos <54008405+Deboradso@users.noreply.github.com>
2020-10-19 14:31:43 -0300

    tickets.vue

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,7 +3,10 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _df386c44 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _17799997 = () => interopDefault(import('..\\pages\\about.vue' /* webpackChunkName: "pages/about" */))
+const _04242db3 = () => interopDefault(import('..\\pages\\poster.vue' /* webpackChunkName: "pages/poster" */))
+const _361f7b71 = () => interopDefault(import('..\\pages\\tickets.vue' /* webpackChunkName: "pages/tickets" */))
+const _3731745c = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -22,8 +25,20 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/about",
+    component: _17799997,
+    name: "about"
+  }, {
+    path: "/poster",
+    component: _04242db3,
+    name: "poster"
+  }, {
+    path: "/tickets",
+    component: _361f7b71,
+    name: "tickets"
+  }, {
     path: "/",
-    component: _df386c44,
+    component: _3731745c,
     name: "index"
   }],
 
