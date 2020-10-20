6423dddcd4d31e440350708da58dcc66d7e45912
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-10-20 07:37:26 -0300

    colocando em negrito
    tag <strong>

--- pages/a_gangue.vue
+++ pages/a_gangue.vue
@@ -8,21 +8,21 @@
     <h1>Título original: TKG: The Kids of Grove</h1>
 
     <p>
-      Direção: Damien Giglietta<br />
-      Gênero: Crime, Suspense<br />
-      Lançamento: 2020<br />
-      Duração: 115 min.
+      <strong> Direção:</strong> Damien Giglietta<br />
+      <strong> Gênero:</strong> Crime, Suspense<br />
+      <strong> Lançamento:</strong> 2020<br />
+      <strong>Duração:</strong> 115 min.
     </p>
     <p>
-      Sinopse: A Wright Firm tem sinal verde para seu maior empreendimento até
-      hoje: ‘O Projeto Grove Street’. Ao transformar um bairro atingido por
-      crimes e pobreza por anos em um ambiente seguro e limpo, a empresa espera
-      receber apoio e compreensão. Este não é o caso. Sua execução e a
-      recuperação de curto prazo deixaram desabrigadas as comunidades já
-      desamparadas. A mídia e o público examinam o projeto pelo que ele fez.
-      Para Nick Wright e sua equipe, espera-se protestos públicos, mas eles
-      continuam a trabalhar horas extras no fim de semana da Páscoa para
-      implementar as coisas. Mal sabem eles, a comunidade de Grove Street
+      <strong> Sinopse:</strong> A Wright Firm tem sinal verde para seu maior
+      empreendimento até hoje: ‘O Projeto Grove Street’. Ao transformar um
+      bairro atingido por crimes e pobreza por anos em um ambiente seguro e
+      limpo, a empresa espera receber apoio e compreensão. Este não é o caso.
+      Sua execução e a recuperação de curto prazo deixaram desabrigadas as
+      comunidades já desamparadas. A mídia e o público examinam o projeto pelo
+      que ele fez. Para Nick Wright e sua equipe, espera-se protestos públicos,
+      mas eles continuam a trabalhar horas extras no fim de semana da Páscoa
+      para implementar as coisas. Mal sabem eles, a comunidade de Grove Street
       prepara uma retaliação de proporções horríveis – e eles já estão chegando.
     </p>
 
