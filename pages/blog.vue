2ca2770d9fcbd5d58c30acbe99defed64f8e3af7
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-10-27 18:26:36 -0300

    card do blog

--- pages/blog.vue
+++ pages/blog.vue
@@ -3,32 +3,40 @@
     <div class="navegacao">
       <p><NuxtLink class="navegacao" to="/">Início</NuxtLink> > Blog</p>
     </div>
-    <div>
-      <b-card no-body class="overflow-hidden" style="max-width: 540px">
-        <b-row no-gutters>
-          <b-col md="6">
-            <b-card-img
-              src="~/assets/img/254.png"
-              alt="Image"
-              class="rounded-0"
-            ></b-card-img>
-          </b-col>
-          <b-col md="6">
-            <b-card-body
-              title=" Assassin's Creed ganha novo filme dessa vez na Netflix"
-            >
-              <b-card-text>
-                A Netflix Brasil posta em seu twitter um video de apenas 10
-                segundos anunciando a nova serie original que entrara
-                futuramente em seu catálogo.
-              </b-card-text>
-            </b-card-body>
-          </b-col>
+
+    <div class="card">
+      <b-card-group deck>
+        <b-card
+          title="Assassin's Creed ganha novo filme dessa vez na Netflix"
+          img-src="~/assets/img/254.png"
+          img-alt="Foto da logo da futura serie da netflix, com o logo do jogo Assassin's creed em vemelho e uma letra N no meio."
+          img-top
+        >
+          <b-card-text>
+            A Netflix Brasil posta em seu twitter um video de apenas 10 segundos
+            anunciando a nova serie original que entrara futuramente em seu
+            catálogo.
+          </b-card-text>
           <b-button class="btn" variant="Primary"
             ><NuxtLink to="/noticia-netflix">Ver mais </NuxtLink></b-button
           >
-        </b-row>
-      </b-card>
+        </b-card>
+
+        <b-card
+          title="Title"
+          img-src="https://picsum.photos/300/300/?image=41"
+          img-alt="Image"
+          img-top
+        >
+          <b-card-text>
+            This card has supporting text below as a natural lead-in to
+            additional content.
+          </b-card-text>
+          <b-button class="btn" variant="Primary"
+            ><NuxtLink to="/">Ver mais </NuxtLink></b-button
+          >
+        </b-card>
+      </b-card-group>
     </div>
   </div>
 </template>
