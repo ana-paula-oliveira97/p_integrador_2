0adfc017a9a7fbd5fdf41c95bc771402c19edf82
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-11-03 14:55:19 -0300

    rodape

--- components/Footer.vue
+++ components/Footer.vue
@@ -37,18 +37,7 @@
         </g>
       </svg>
     </NuxtLink>
-    <div class="map">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14858.39169020325!2d-48.5052161!3d-21.4057308!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc662afc91651067!2sCinemec%20Taquaritinga!5e0!3m2!1spt-BR!2sbr!4v1602610559177!5m2!1spt-BR!2sbr"
-        width="400"
-        height="300"
-        frameborder="0"
-        style="border: 0"
-        allowfullscreen=""
-        aria-hidden="false"
-        tabindex="0"
-      ></iframe>
-    </div>
+
     <p>Endereço: R. da República, 999-897, Taquaritinga - SP, 15900-000</p>
     <p class="fim-do-rodape">©Todos os direitos reservados.</p>
   </div>
